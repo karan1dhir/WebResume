72efe583aaffc9cc94ceefcc4e7fd5cb13777453
karan1dhir <karandhir64@yahoo.co.in>
2022-07-03 22:47:46 -0500

    bugfix

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          adding some changes to view in heroku
         </p>
         <a
           className="App-link"
